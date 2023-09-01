e5f4be99b36a473cc08b07e5fc350c9e907d08d0
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-01 21:53:28 +0800

    Added code to merge all extracted  raw data value into one single dataframe

--- export-preparation-df.py
+++ export-preparation-df.py
@@ -16,3 +16,23 @@ for raw in data_raw:
         else:
             individual_data.append(cols)
     data_extracted.append(individual_data)
+    
+start_time = time.time()
+
+# Select a subset of columns from the first row as column names
+raw_df = pd.DataFrame()
+
+for item in data_extracted:
+    df = pd.DataFrame(item)
+    for index, row in df.iterrows():
+        row_data = list(row)
+        if "GRAND TOTAL" in row_data:
+            i = index
+    df.drop(df.index[i], inplace=True)
+    raw_df = pd.concat([raw_df, df], axis=1, ignore_index=True)
+    
+raw_df = raw_df.loc[:, ~raw_df.columns.duplicated()]
+display(raw_df)
+
+end_time = time.time()
+print(f'{end_time-start_time}')
