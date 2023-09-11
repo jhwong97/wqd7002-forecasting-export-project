bcf2143f3c643ce810090a075a8a70a7269da38f
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-11 17:52:43 +0800

    Added some comments on the part to be changed.

--- wbg-extraction-raw.py
+++ wbg-extraction-raw.py
@@ -9,9 +9,15 @@ for retry in range(max_retries):
         try:
             data_raw = wb.data.DataFrame(['NY.GDP.MKTP.KN', 'SP.POP.TOTL', 'PA.NUS.FCRF', 'PX.REX.REER'], 'MYS', timeColumns=True, numericTimeKeys=True).transpose().reset_index()
             print("Data from WBG has been extracted successfully.")
-            print(data_raw)
+            
+            col_names = ['YEAR', 'GDP', 'EXCHANGE RATE', 'REER', 'POPULATION']
+            
+            # Renaming column names
+            data_raw.columns = col_names
             break
         
         except Exception as e:
             print(f"Error: {e}. Retrying in {retry_delay} seconds...")
             time.sleep(retry_delay)
+            
+print(data_raw)
