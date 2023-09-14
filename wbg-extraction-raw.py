8be3fb2480e72c7b8097d873f62b02f049e375b0
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-14 20:55:11 +0800

    Create a function to extract data from wbg api

--- wbg-extraction-raw.py
+++ wbg-extraction-raw.py
@@ -1,23 +1,19 @@
 import wbgapi as wb
-import random
-import time
+from logging_export import logger
 
-max_retries = 2
+def wbg_extract(datacode, country):
+    
+    MAX_RETRIES = 2
+    RETRY_DELAY = 5
 
-for retry in range(max_retries):
-        retry_delay = random.randint(10,15)
-        try:
-            data_raw = wb.data.DataFrame(['NY.GDP.MKTP.KN', 'SP.POP.TOTL', 'PA.NUS.FCRF', 'PX.REX.REER'], 'MYS', timeColumns=True, numericTimeKeys=True).transpose().reset_index()
-            print("Data from WBG has been extracted successfully.")
+    for retry in range(MAX_RETRIES):
+            try:
+                wbg_raw_df = wb.data.DataFrame(datacode, country, timeColumns=True, numericTimeKeys=True).transpose().reset_index()                  
+                logger.info("Data from WBG has been extracted successfully.")
+                break
             
-            col_names = ['YEAR', 'GDP', 'EXCHANGE RATE', 'REER', 'POPULATION']
-            
-            # Renaming column names
-            data_raw.columns = col_names
-            break
-        
-        except Exception as e:
-            print(f"Error: {e}. Retrying in {retry_delay} seconds...")
-            time.sleep(retry_delay)
-            
-print(data_raw)
+            except Exception as e:
+                print(f"Error: {e}. Retrying in {RETRY_DELAY} seconds...")
+                time.sleep(RETRY_DELAY)
+                
+    return wbg_raw_df
