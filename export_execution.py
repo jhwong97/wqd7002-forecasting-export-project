f954a1586fd1912a9d92e20cd012f0a23f83d782
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-14 17:59:50 +0800

    Added main execution code for export data

---
+++ export_execution.py
@@ -0,0 +1,10 @@
+from export_input import user_input
+from export_raw_extract import export_extract
+from export_preparation import export_prepare
+
+result = user_input()
+start_year = result[0]
+end_year = result[1]
+
+raw_data = export_extract(start_year, end_year)
+export_prepare(raw_data)
