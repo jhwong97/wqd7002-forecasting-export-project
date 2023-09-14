e9347676f935650b3dc2dfb31eb8302a806111b1
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-14 17:20:36 +0800

    Created a function for user input

---
+++ export_input.py
@@ -0,0 +1,20 @@
+from logging_export import logger
+
+def user_input():
+    while True:
+        try:
+            start_year, end_year = input("Please enter the range of year to be extracted (E.g, 2000-2012): ").split("-")
+            start_year = int(start_year.strip())
+            end_year = int(end_year.strip())
+            if end_year < start_year:
+                logger.info("Please ensure the given range of year is in the right order.")
+            else:
+                break
+        
+        except ValueError:
+            logger.info("Sorry, your input is not following to the above mentioned format.")
+            continue
+        
+    return start_year, end_year
+
+user_input()
