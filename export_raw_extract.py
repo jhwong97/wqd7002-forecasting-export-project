bf4760af1981e641953b0aed119caf355755fd1a
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-09-25 18:07:11 +0800

    Converts url and payload to params, shift all input params to main execution file

--- export_raw_extract.py
+++ export_raw_extract.py
@@ -3,46 +3,9 @@ from logging_export import logger
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
-import os
-
-load_dotenv()
-token = os.getenv('csrf_token')
-cookie = os.getenv('cookie')
-
-# csrf_token and headers for targeted site
-csrf_token = token
-headers = {
-"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", 
-"Cookie": cookie} 
 
 # Function to extract data
-def export_extract(start_year, end_year):
-    URL = 'https://metsonline.dosm.gov.my/tradev2/product-coderesult'
-    
-    payload_month = {
-    '_csrf': csrf_token,
-    'Tradev2[typeofsearch]': 'classification',
-    'Tradev2[typedigit]': 7,
-    'Tradev2[rangecode1]': 0,
-    'Tradev2[rangecode2]': 9,
-    # 'Tradev2[code_idcode]': ,
-    # 'Tradev2[code_idcodedigit9]': ,
-    # 'Tradev2[tradeflow]': ,
-    'Tradev2[tradeflow][]': 'exports',
-    # 'Tradev2[timeframe]': ,
-    'Tradev2[timeframe]': 'month',
-    # 'Tradev2[rangeyear]': ,
-    # 'Tradev2[rangeyear2]': ,
-    # 'Tradev2[rangeyearone]': ,
-    # 'Tradev2[rangemonthone]': ,
-    'Tradev2[mothdata]': start_year,
-    'Tradev2[mothdata2]': end_year,
-    # 'Tradev2[classification_serch]': ,
-    # 'Tradev2[country2]': ,
-    'Tradev2[geogroup]': 1,
-    'Tradev2[geogroup]': 29,
-    'Tradev2[codeshowby]': 'code'
-    }
+def export_extract(url, payload, headers ,start_year, end_year):
     
     MAX_RETRIES = 2
     
@@ -62,7 +25,7 @@ def export_extract(start_year, end_year):
     session.mount('https://', adapter)
     
     # Make a request using the session object
-    raw_data = session.post(URL, data=payload_month, headers=headers)    
+    raw_data = session.post(url, data=payload, headers=headers)    
     
     if raw_data.status_code == 200:
         logger.info(f"SUCCESS: Data for {end_year} has been extracted")
