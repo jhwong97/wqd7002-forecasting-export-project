0b81ecaa09b5a6dcbace86fe08387725936eb03a
JH Wong <102636360+jhwong97@users.noreply.github.com>
2023-10-04 20:49:43 +0800

    Removal of HTML parsing, removal of unnecessary data prior to dataframe conversion

--- export_preparation.py
+++ export_preparation.py
@@ -1,29 +1,27 @@
 from export_raw_extract import export_extract
 from logging_export import logger
 import pandas as pd
-from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 def export_prepare(raw_data):
     
-    result = BeautifulSoup(raw_data.text, 'html.parser') # Parse the HTML
-    table = result.find('table', class_='table-bordered') # Look up for the table
+    result = raw_data.find('table', class_='table-bordered') # Look up for the table
     # Extract table rows
-    rows = table.find_all('tr')
+    if not isinstance(result, Tag):
+        return
+    
+    rows = result.find_all('tr')
     
     individual_data = []
     for row in rows:
-        cols = row.find_all(['th', 'td'])
-        cols = [col.get_text(strip=True) for col in cols]
-        if cols:
-            individual_data.append(cols)
+        data = row.find_all(['th', 'td'])
+        if data:
+            data = [item.get_text(strip=True) for item in data]
+            if 'GRAND TOTAL' not in data:
+                individual_data.append(data)
 
-    # Select a subset of columns from the first row as column names
+    logger.info('Preparing raw to dataframe...... ')
     df = pd.DataFrame(individual_data)
-    for index, row in df.iterrows():
-        row_data = list(row)
-        if "GRAND TOTAL" in row_data:
-            i = index
-    df.drop(df.index[i], inplace=True)
-    logger.info(df)
+    logger.info('SUCCESS: Dataframe has been created.')
     
     return df
