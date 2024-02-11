7360ce42512105c9841884a092acdbb00a390793
JH Wong <102636360+jhwong97@users.noreply.github.com>
2024-02-11 21:34:35 +0800

    Added task2 for data transformation.

--- dags/imf_etl_dag.py
+++ dags/imf_etl_dag.py
@@ -30,6 +30,19 @@ bucket_name_t1 = "wqd7002_project"
 blob_name_t1 = ["world_export_raw_data.csv"]
 file_format_t1 = "csv"
 
+# Task 2
+def imf_transformation_ti(ti, client, bucket_name, blob_name, file_format):
+    gcs_uri_list_raw = ti.xcom_pull(task_ids = "raw_data_extract")
+    gcs_uri_list = imf_transformation(gcs_uri_list=gcs_uri_list_raw,
+                                      client=client,
+                                      bucket_name=bucket_name,
+                                      blob_name=blob_name,
+                                      file_format=file_format)
+    return gcs_uri_list
+
+blob_name_t2 = ["world_export_transformed_data.csv"]
+file_format_t2 = "csv"
+
 # Dag configurations part
 default_args = {
     'owner': 'albert',
@@ -76,4 +89,13 @@ with DAG(
                    "file_format": file_format_t1,}
     )
     
-    task1
+    task2 = PythonOperator(
+        task_id='data_transformation',
+        python_callable=imf_transformation_ti,
+        op_kwargs={"client": storage_client,
+                   "bucket_name": bucket_name_t1,
+                   "blob_name": blob_name_t2,
+                   "file_format": file_format_t2,}
+    )
+    
+    task1 >> task2
